77333f1f1d46165fee7d740c2ea3cdcf9ec8bd5b
Dan Wright <muscle.crab.13@gmail.com>
2015-04-12 16:15:52 -0700

    enable basic access to blogs served by rails through generated controllers

--- app/controllers/blog.js
+++ app/controllers/blog.js
@@ -1,10 +1,7 @@
-// import Ember from 'ember';
-//
-// export default Ember.Controller.extend({
-//
-//   kindIsEstate: function() {
-//     return this.get('model.kind') === 'Estate Sale';
-//   }.property('kind'),
-//
-//
-// });
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+
+
+});

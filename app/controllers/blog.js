3babece25b08e9eae9e3a2e8a43d289d75f309a7
Dan Wright <muscle.crab.13@gmail.com>
2015-04-14 17:22:22 -0700

    add full instantaneous CRUD for blogs

--- app/controllers/blog.js
+++ app/controllers/blog.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
+import EmberPusher from 'ember-pusher';
 
-export default Ember.Controller.extend({
-
-
+export default Ember.Controller.extend(EmberPusher.Bindings, {
+  logPusherEvents: true,
+  PUSHER_SUBSCRIPTIONS: {
+    blogsChannel: ['edit-blog']
+  },
+  actions: {
+    editBlog: function(payload) {
+      console.log(payload);
+      this.store.pushPayload('blog', payload);
+    }
+  }
 
 });
--- app/controllers/blogs.js
+++ app/controllers/blogs.js
@@ -4,12 +4,18 @@ import EmberPusher from 'ember-pusher';
 export default Ember.Controller.extend(EmberPusher.Bindings, {
   logPusherEvents: true,
   PUSHER_SUBSCRIPTIONS: {
-    blogsChannel: ['new-blog']
+    blogsChannel: ['new-blog', 'delete-blog' ]
   },
   actions: {
     newBlog: function(payload) {
       console.log(payload);
       this.store.pushPayload('blog', payload);
+    },
+    deleteBlog: function(payload) {
+      console.log(payload.blog.id);
+      this.store.find('blog', payload.blog.id).then(function (blog) {
+        blog.deleteRecord();
+      });
     }
   }
 

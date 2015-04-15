ae2398d52cdc6dd814f23939b3488a24efa022f6
Dan Wright <muscle.crab.13@gmail.com>
2015-04-15 09:57:58 -0700

    update styling

--- config/environment.js
+++ config/environment.js
@@ -21,7 +21,7 @@ module.exports = function(environment) {
     'script-src': "'self' https://cdn.mxpnl.com http://stats.pusher.com", // Allow scripts from https://cdn.mxpnl.com
     'font-src': "'self' http://maxcdn.bootstrapcdn.com", // Allow fonts to be loaded from http://fonts.gstatic.com
     'connect-src': "'self' https://api.mixpanel.com http://custom-api.local ws://ws.pusherapp.com", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
-    'img-src': "'self'",
+    'img-src': "'self' http://i0.kym-cdn.com www.unsigneddesign.com",
     'style-src': "'self' 'unsafe-inline' http://maxcdn.bootstrapcdn.com",
     'media-src': "'self'"
   },
